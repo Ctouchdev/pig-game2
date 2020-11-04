af37ead02f0159993f46ff7b68206df646adc279
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-11-04 12:29:17 -0600

    Changed random var to ceil

--- pig/library_die.js
+++ pig/library_die.js
@@ -1,6 +1,6 @@
 var Die = function() {};
 Die.prototype.rollDie = function() {
     var random = Math.random();
-    random = Math.floor(random * 6);
+    random = Math.ceil(random * 20);
     return random + 1;
 };
